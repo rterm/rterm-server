ec9033da49ce67caaab06be8643c95d43eec9ff8
Ly Nguyen <lyntc.elec@gmail.com>
2020-12-28 22:00:54 +0700

    - Add API passport Google, local login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,22 +1,76 @@
-import * as status from "../const/status";
+import * as status from "../constants/status";
 const express = require("express");
 import logger from "../utils/logger";
 const NodePassport = require("../passport/setup");
 const router = express.Router();
+import * as mail from "../utils/mail";
+import * as encryption from "../utils/encryption";
+import * as jwttoken from "../utils/JwtToken";
+const { User } = require("../utils/db");
 
-router.get("/login", function (req: any, res: any, next: any) {
-  console.log("user", req.user);
-  res.end("OK");
+router.post("/signup", async function (req: any, res: any) {
+  if (req.body.password !== req.body.confirm_password) {
+    return res.json({
+      status: status.PASS_NOT_MATCHED,
+      msg: "Password confirm do not match",
+    });
+  }
+
+  const checkuser = await User.findOne({ where: { email: req.body.email } });
+  if (checkuser !== null) {
+    if (checkuser.emailVerified) {
+      return res.json({
+        status: status.EMAIL_ALREADY_EXISTS,
+        msg: "Email already exists and register successfully",
+      });
+    } else {
+      await checkuser.destroy();
+    }
+  }
+
+  const emailVerifiedId = encryption.genid().replace(/-/g, "");
+
+  const user = new User();
+  user.displayName = req.body.display_name;
+  user.givenName = req.body.given_name;
+  user.familyName = req.body.family_name;
+  user.email = req.body.email;
+  user.password = req.body.password;
+  user.emailVerifiedId = emailVerifiedId;
+  await user.save();
+
+  // const content = `
+  // <h2 style="color: #2e6c80;">Click on this link to verify your email:</h2>
+  // <p><strong>&nbsp;<a href="${process.env.BASE_URL}/verifyemail?id=${emailVerifiedId}">${process.env.BASE_URL}/verifyemail?id=${emailVerifiedId}</a></strong></p>
+  // `;
+  // mail.send(req.body.email, "iLogger Email Verification", content);
+
+  if (checkuser !== null && checkuser.emailVerified === false) {
+    return res.json({
+      status: status.UNKNOWN,
+      msg: "Email already exists but has not been verified, please check your mailbox",
+    });
+  } else {
+    return res.json({
+      status: status.SUCCESS,
+      msg: "Signup successfully, please check your mailbox to verify your email",
+    });
+  }
 });
 
+router.post(
+  "/login",
+  NodePassport.authenticate("local", {
+    successRedirect: `${process.env.API_PATH}/auth/local/success`,
+    failureRedirect: `${process.env.API_PATH}/auth/local/failure`,
+  }),
+);
+
 router.get("/user", async function (req: any, res: any) {
-  logger.info(req.session);
-  logger.info(req.user);
-  logger.info(req.session.passport);
-  logger.info(req.session.user);
   return res.json({
     status: status.SUCCESS,
     msg: "user",
+    user: req.user.jwt,
   });
 });
 
@@ -30,10 +84,44 @@ router.get(
   }),
 );
 
+router.get("/local/success", async function (req: any, res: any) {
+  if (req.user.emailVerified === false) {
+    return res.json({
+      status: status.UNKNOWN,
+      msg: "Email already exists but has not been verified, please check your mailbox",
+    });
+  } else {
+    req.user.bio = null;
+    req.user.image = null;
+    req.user.jwt = jwttoken.toAuthJSON(req.user);
+    return res.json({
+      status: 0,
+      msg: "Local login successfully",
+      data: {
+        user: req.user.jwt,
+      },
+    });
+  }
+});
+
+router.get("/local/failure", async function (req: any, res: any) {
+  return res.json({
+    status: status.ERROR,
+    code: status.GG_OAUTH_FAILED,
+    msg: "Local login error",
+  });
+});
+
 router.get("/google/success", async function (req: any, res: any) {
+  req.user.bio = null;
+  req.user.image = null;
+  req.user.jwt = jwttoken.toAuthJSON(req.user);
   return res.json({
     status: 0,
     msg: "Google login successfully",
+    data: {
+      user: req.user.jwt,
+    },
   });
 });
 
@@ -41,7 +129,7 @@ router.get("/google/failure", async function (req: any, res: any) {
   return res.json({
     status: status.ERROR,
     code: status.GG_OAUTH_FAILED,
-    msg: "Login error",
+    msg: "Google login error",
   });
 });
 
